be731d4863f56e3c3f1e4b8abe6f5ed436193bb3
Miko <miko@Mikos-MacBook-Pro.local>
2019-12-13 11:35:54 +0000

    orders

--- src/contracts/Exchange.sol
+++ src/contracts/Exchange.sol
@@ -4,22 +4,53 @@ import 'openzeppelin-solidity/contracts/math/SafeMath.sol';
 import './Token.sol';
 
 contract Exchange {
-  using SafeMath for uint;
+  using SafeMath for uint256;
 
   address public feeAccount; // the acc that receives exchange fees
   uint256 public feePercent;
   address constant ETHER = address(0); // store ether in tokens mapping with blank address
   mapping(address => mapping(address => uint256)) public tokens; // storeTokens infos: tokens[tokenAddr][user1] = 10E
+  mapping(uint256 => _Order) public orders;
+  uint256 public orderCount;
+  mapping(uint256 => bool) public orderCanceled;
 
   event Deposit(address token, address user, uint256 amount, uint256 balance);
   event Withdraw(address token, address user, uint256 amount, uint256 balance);
+  event Order(
+    uint256 id,
+    address user,
+    address tokenGet,
+    uint256 amountGet,
+    address tokenGive,
+    uint256 amountGive,
+    uint256 timestamp
+  );
+  event Cancel(
+    uint256 id,
+    address user,
+    address tokenGet,
+    uint256 amountGet,
+    address tokenGive,
+    uint256 amountGive,
+    uint256 timestamp
+  );
+
+  struct _Order {
+    uint256 id;
+    address user;
+    address tokenGet;
+    uint256 amountGet;
+    address tokenGive;
+    uint256 amountGive;
+    uint256 timestamp;
+  }
 
   constructor (address _feeAccount, uint256 _feePercent) public {
     feeAccount = _feeAccount;
     feePercent = _feePercent;
   }
 
-  // if ehter is sent to this contract by mistake ???
+  // if ehter is sent to this contract by mistake. Doesnt work in test???
   function() external {
     revert();
   }
@@ -29,14 +60,14 @@ contract Exchange {
     emit Deposit(ETHER, msg.sender, msg.value, tokens[ETHER][msg.sender]);
   }
 
-  function withdrawEther(uint _amount) public {
+  function withdrawEther(uint256 _amount) public {
     require(tokens[ETHER][msg.sender] >= _amount);
     tokens[ETHER][msg.sender] = tokens[ETHER][msg.sender].sub(_amount);
     msg.sender.transfer(_amount); // msg.sender = user1 . transfer . from address(this) = exchange
     emit Withdraw(ETHER, msg.sender, _amount, tokens[ETHER][msg.sender]);
   }
 
-  function depositToken(address _token, uint _amount) public {
+  function depositToken(address _token, uint256 _amount) public {
     require(_token != ETHER);
     require(Token(_token).transferFrom(msg.sender, address(this), _amount)); //transferFrom(user1, exchange, 10)
     tokens[_token][msg.sender] = tokens[_token][msg.sender].add(_amount); // tokens[tokenAddr][user1] = 10E
@@ -54,4 +85,24 @@ contract Exchange {
   function balanceOf(address _token, address _user) public view returns (uint256) {
     return tokens[_token][_user];
   }
+
+  function makeOrder(address _tokenGet, uint256 _amountGet, address _tokenGive, uint256 _amountGive) public {
+    orderCount = orderCount.add(1);
+    orders[orderCount] = _Order(orderCount, msg.sender, _tokenGet, _amountGet, _tokenGive, _amountGive, now);
+    emit Order(orderCount, msg.sender, _tokenGet, _amountGet, _tokenGive, _amountGive, now);
+  }
+
+  function cancelOrder(uint256 _id) public {
+    _Order storage _order = orders[_id];
+    require(address(_order.user) == msg.sender);
+    require(_order.id == _id); //order must exists
+    orderCanceled[_id] = true;
+    emit Cancel(_order.id, msg.sender, _order.tokenGet, _order.amountGet, _order.tokenGive, _order.amountGive, now);
+  }
+
+  function fillOrder(uint256 _id) public {
+      _Order storage _order = orders[_id];
+
+  }
+
 }
--- test/Exchange.test.js
+++ test/Exchange.test.js
@@ -7,7 +7,7 @@ require('chai')
   .use(require('chai-as-promised'))
   .should()
 
-contract('Exchange', ([deployer, feeAccount, user1]) => {
+contract('Exchange', ([deployer, feeAccount, user1, user2]) => {
   let exchange
   let token
   const feePercent = 10
@@ -36,7 +36,6 @@ contract('Exchange', ([deployer, feeAccount, user1]) => {
       await exchange.sendTransaction({ value: 1, from: user1 }).should.be.rejectedWith(EVM_REVERT)
     })
   })
-
 
   describe('depositing Ether', async () => {
     let result
@@ -202,6 +201,89 @@ contract('Exchange', ([deployer, feeAccount, user1]) => {
     it('return user balance', async () => {
       const result = await exchange.balanceOf(ETHER_ADDRESS, user1)
       result.toString().should.equal(ether(1).toString())
+    })
+  })
+
+  describe('making orders', async () => {
+    let result
+
+    beforeEach(async () => {
+      result = await exchange.makeOrder(token.address, tokens(1), ETHER_ADDRESS, ether(1), { from: user1 })
+    })
+
+    it('tracks the newly created order', async () => {
+      const orderCount = await exchange.orderCount()
+      orderCount.toString().should.equal('1')
+      const order = await exchange.orders('1')
+      order.id.toString().should.equal('1')
+      order.user.should.equal(user1)
+      order.tokenGet.should.equal(token.address)
+      order.amountGet.toString().should.equal(tokens(1).toString())
+      order.tokenGive.should.equal(ETHER_ADDRESS)
+      order.amountGive.toString().should.equal(ether(1).toString())
+      order.timestamp.toString().length.should.be.at.least(1)
+    })
+
+    it('emits an "Order event', async () => {
+      const log = result.logs[0]
+      log.event.should.eq('Order')
+      const event = log.args
+      event.id.toString().should.equal('1')
+      event.user.should.equal(user1)
+      event.tokenGet.should.equal(token.address)
+      event.amountGet.toString().should.equal(tokens(1).toString())
+      event.tokenGive.should.equal(ETHER_ADDRESS)
+      event.amountGive.toString().should.equal(ether(1).toString())
+      event.timestamp.toString().length.should.be.at.least(1)
+    })
+  })
+
+  describe('order actions', async () => {
+    beforeEach(async () => {
+      //user1 deposits ether
+      await exchange.depositEther({ from: user1, value: ether(1) })
+      //user1 makes an order to buy tokens with Ether
+      await exchange.makeOrder(token.address, tokens(1), ETHER_ADDRESS, ether(1), { from: user1 })
+    })
+
+    describe('cancelling orders', async () => {
+      let result
+
+      describe('success', async () => {
+        beforeEach(async () => {
+          result = await exchange.cancelOrder('1', { from: user1 })
+        })
+
+        it('updates cancelled orders', async () => {
+          const orderCanceled = await exchange.orderCanceled(1)
+          orderCanceled.should.equal(true)
+        })
+
+        it('emits a "Cancel" event', async () => {
+          const log = result.logs[0]
+          log.event.should.eq('Cancel')
+          const event = log.args
+          event.id.toString().should.equal('1')
+          event.user.should.equal(user1)
+          event.tokenGet.should.equal(token.address)
+          event.amountGet.toString().should.equal(tokens(1).toString())
+          event.tokenGive.should.equal(ETHER_ADDRESS)
+          event.amountGive.toString().should.equal(ether(1).toString())
+          event.timestamp.toString().length.should.be.at.least(1)
+        })
+      })
+
+      describe('failure', async () => {
+        it('rejects invalid order ids', async () => {
+          const invalidOrderId = 9999
+          await exchange.cancelOrder(invalidOrderId, { from: user1 }).should.be.rejectedWith(EVM_REVERT)
+        })
+
+        it('rejects unauthorized cancelations', async () => {
+          // try to cancel the order from another user
+          await exchange.cancelOrder('1', { from: user2 }).should.be.rejectedWith(EVM_REVERT)
+        })
+      })
     })
   })
 })
